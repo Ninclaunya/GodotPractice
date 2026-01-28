15a59b9abf85148e0a3ef4f036f6145d2f61ecc8
Ninclaunya <nindie0304@gmail.com>
2026-01-28 18:17:25 +0700

    Tambahan
    Damage gk dispam
    Dialog Dikit

--- enemy.gd
+++ enemy.gd
@@ -22,16 +22,23 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	and event.button_index == MOUSE_BUTTON_LEFT \
 	and event.pressed:
 		take_damage(click_damage)
+		
 
 func take_damage(amount):
 	currentHealth -= amount
 	isHurt = true
 	healthChanged.emit()
 	isHurt = false
-	
+	if currentHealth <= 0:
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/Die.dialogue"), "start")
 
 func deal_damage(player):
 	if can_damage:
 		player.hurt(self)
+		can_damage = false
+		$"../TakeDamage".start()
 
+func _on_take_damage_timeout() -> void:
+	
+	can_damage = true
 	
--- player.gd
+++ player.gd
@@ -8,11 +8,13 @@ signal healthChanged
 @export var maxHealth := 100
 @onready var currentHealth := maxHealth
 
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+var temp := true
 var isHurt := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/Welcome.dialogue"), "start")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,6 +27,14 @@ func _process(delta: float) -> void:
 		var body = collision.get_collider()
 		if body.has_method("deal_damage"):
 			body.deal_damage(self)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("click"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0 and temp:
+			temp = false
+			DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/Chat.dialogue"), "start")
+			return
 
 func hurt(area):
 	currentHealth -= 10
